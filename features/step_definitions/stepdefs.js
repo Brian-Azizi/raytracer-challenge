1e83013248850569a653312e16b95ac12277abcc
Brian Azizi <briana@theodo.co.uk>
2020-03-07 13:47:55 +0000

    Implement tuple

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,22 +1,31 @@
 const assert = require("assert");
 const { Given, When, Then } = require("cucumber");
+const { tuple } = require("../../src/tuple");
 
-function isItFriday(today) {
-  if (today === "Friday") {
-    return "TGIF";
-  } else {
-    return "Nope";
-  }
-}
-
-Given("today is {string}", function(givenDay) {
-  this.today = givenDay;
+Given("{word} <- tuple\\({float}, {float}, {float}, {float})", function(
+  variable,
+  float,
+  float2,
+  float3,
+  float4
+) {
+  this[variable] = tuple(float, float2, float3, float4);
 });
 
-When("I ask whether it's Friday yet", function() {
-  this.actualAnswer = isItFriday(this.today);
+Then("{word}.{word} = {float}", function(variable, property, float) {
+  assert.equal(this[variable][property], float);
 });
 
-Then("I should be told {string}", function(expectedAnswer) {
-  assert.equal(this.actualAnswer, expectedAnswer);
+Then("{word} is a point", function(variable) {
+  assert.equal(this[variable].w, 1.0);
+});
+Then("{word} is not a point", function(variable) {
+  assert.notEqual(this[variable].w, 1.0);
+});
+
+Then("{word} is a vector", function(variable) {
+  assert.equal(this[variable].w, 0);
+});
+Then("{word} is not a vector", function(variable) {
+  assert.notEqual(this[variable].w, 0);
 });
---
+++ src/tuple.js
@@ -0,0 +1,5 @@
+const tuple = (x, y, z, w) => ({ x, y, z, w });
+
+module.exports = {
+  tuple
+};
