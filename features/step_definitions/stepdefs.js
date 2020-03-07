79dd0e5c58f6b2261fad89e4ba663a4e9b9c9ed6
Brian Azizi <briana@theodo.co.uk>
2020-03-07 14:09:06 +0000

    Implement point and vector

--- features/step_definitions/stepdefs.js
+++ features/step_definitions/stepdefs.js
@@ -1,15 +1,19 @@
 const assert = require("assert");
 const { Given, When, Then } = require("cucumber");
-const { tuple } = require("../../src/tuple");
+const { tuple, point, vector } = require("../../src/tuple");
 
-Given("{word} <- tuple\\({float}, {float}, {float}, {float})", function(
+Given("{word} {word} tuple\\({float}, {float}, {float}, {float})", function(
   variable,
+  comparator,
   float,
   float2,
   float3,
   float4
 ) {
-  this[variable] = tuple(float, float2, float3, float4);
+  if (comparator === "<-")
+    this[variable] = tuple(float, float2, float3, float4);
+  else if (comparator === "=")
+    assert.deepEqual(this[variable], tuple(float, float2, float3, float4));
 });
 
 Then("{word}.{word} = {float}", function(variable, property, float) {
@@ -28,4 +32,22 @@ Then("{word} is a vector", function(variable) {
 });
 Then("{word} is not a vector", function(variable) {
   assert.notEqual(this[variable].w, 0);
+});
+
+Given("{word} <- point\\({float}, {float}, {float})", function(
+  variable,
+  float,
+  float2,
+  float3
+) {
+  this[variable] = point(float, float2, float3);
+});
+
+Given("{word} <- vector\\({float}, {float}, {float})", function(
+  variable,
+  float,
+  float2,
+  float3
+) {
+  this[variable] = vector(float, float2, float3);
 });
--- src/tuple.js
+++ src/tuple.js
@@ -1,5 +1,8 @@
 const tuple = (x, y, z, w) => ({ x, y, z, w });
-
+const point = (x, y, z) => tuple(x, y, z, 1);
+const vector = (x, y, z) => tuple(x, y, z, 0);
 module.exports = {
-  tuple
+  tuple,
+  point,
+  vector
 };
